798309d80e83ebd1bb0cf91128a1ce3275ba1510
LuizQuirino <luizfpq@gmail.com>
2023-11-12 23:47:39 -0300

    fix creating disable_suspension.py

--- notebook_home_server/disable_suspension.py
+++ notebook_home_server/disable_suspension.py
@@ -1,5 +1,6 @@
 import subprocess
 
+
 # Função para executar comandos do terminal e confirmar o sucesso
 def run_command_with_confirmation(command, confirmation_message):
     try:
@@ -9,9 +10,28 @@ def run_command_with_confirmation(command, confirmation_message):
         print(f"Error: {confirmation_message}")
         print(e)
 
-# Comandos a serem executados
+
+# Content for disable-lid-suspend.service
+content = '''[Unit]
+Description=Disable Lid Suspend
+
+[Service]
+Type=oneshot
+ExecStart=/bin/bash -c 'echo "HandleLidSwitch=ignore" >> /etc/systemd/logind.conf'
+ExecStart=/bin/systemctl restart systemd-logind
+
+[Install]
+WantedBy=multi-user.target
+'''
+
+# Command list
 commands = [
-    ("echo 'HandleLidSwitch=ignore' | sudo tee -a /etc/systemd/logind.conf", "Adicionando configuração para ignorar fechamento da tampa"),
+    ("echo 'HandleLidSwitch=ignore' | sudo tee -a /etc/systemd/logind.conf",
+     "Add:HandleLidSwitch=ignore -> logind.conf"),
+    ("sudo touch /etc/systemd/system/disable-lid-suspend.service",
+     "Creating: disable-lid-suspend.service"),
+    ("echo {} | sudo tee -a /etc/systemd/logind.conf",
+     "Add: content -> disable-lid-suspend.service".format(content)),
     ("sudo systemctl restart systemd-logind", "Restarting systemd-logind"),
     ("sudo systemctl daemon-reload", "Reloading systemd services"),
     ("sudo systemctl enable disable-lid-suspend.service", "Enabling disable-lig-suspend.service"),
